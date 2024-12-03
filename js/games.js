a07943146d9cde811e8e41ca00681a01d0936e16
Aidan Smith <originalaidan@gmail.com>
2024-12-03 13:04:11 -0700

    update images on game select

--- js/games.js
+++ js/games.js
@@ -1,23 +1,23 @@
 let gameCardData = [
     {
-        gameId: "game1",
-        name: "Sea Otter vs Communist Zombies",
-        image: "https://placehold.co/400x400",
-        description: "Sea Otter vs Communist Zombies is a 90s-style 2D platformer with intense boss battles and retro visuals. A must-play game!",
-        price: 89.99
-    }, 
-    {
         gameId: "game2",
         name: "Demon King Kong vs The World",
-        image: "https://placehold.co/400x400",
+        image: "img/kongCover.png",
         description: "King Kong vs The World is a story-driven game where you battle King Kong in 2035, facing intense challenges to save humanity.",
         price: 89.99
     },
     {
+        gameId: "game1",
+        name: "Sea Otter vs Communist Zombies",
+        image: "img/otterNoText.jpg",
+        description: "Sea Otter vs Communist Zombies is a 90s-style 2D platformer with intense boss battles and retro visuals. A must-play game!",
+        price: 89.99
+    }, 
+    {
         gameId: "game3",
         name: "Clyde the Capybara: Guardian of the Riverbank",
-        image: "https://placehold.co/400x400",
-        description: "Clyde the capybara defends his Amazon River bank from predators, battling jaguars, anacondas, and pesky pelicans.",
+        image: "img/Capybara.jpg",
+        description: "Clyde the capybara defends his Amazon River bank from predators, jaguars, anacondas, and pesky pelicans.",
         price: 89.99
     },
 ];
